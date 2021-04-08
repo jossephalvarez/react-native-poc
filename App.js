6184307abcd134669557f0f688836b3bdd299790
Josseph <jossephalvarez@gmail.com>
2021-04-08 15:40:25 +0200

    feat(): authentication added, config added to generate APK

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,15 +7,27 @@ import LocationScreen from './screens/LocationScreen';
 import CameraScreen from './screens/CameraScreen';
 import WebviewScreen from './screens/WebviewScreen';
 import BiometricScreen from './screens/BiometricScreen';
-// import {LocationScreen,CameraScreen, WebviewScreen } from './screens/index'
-
+// import {LocationScreen,CameraScreen, WebviewScreen } from './screens/index';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 const LOCATION='Location', CAMERA ='Camera', WEBVIEW='Webview', BIOMETRIC= 'Biometric';
+
+const getToken = async () =>{
+    const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    console.log('status',status);
+    if(status !=="granted"){
+        return;
+    }
+    const token= await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+
+}
 
 function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Welcome!!</Text>
             <Button
                 title="Go to Location"
                 onPress={() => navigation.navigate(LOCATION)}
@@ -38,18 +50,22 @@ function HomeScreen({ navigation }) {
 
 const Stack = createStackNavigator();
 
-function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name={LOCATION} component={LocationScreen} />
-                <Stack.Screen name={CAMERA} component={CameraScreen} />
-                <Stack.Screen name={WEBVIEW} component={WebviewScreen} />
-                <Stack.Screen name={BIOMETRIC} component={BiometricScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default class App extends Component{
+    componentDidMount() {
+        getToken();
+    }
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                    <Stack.Screen name={LOCATION} component={LocationScreen}/>
+                    <Stack.Screen name={CAMERA} component={CameraScreen}/>
+                    <Stack.Screen name={WEBVIEW} component={WebviewScreen}/>
+                    <Stack.Screen name={BIOMETRIC} component={BiometricScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
-export default App;
